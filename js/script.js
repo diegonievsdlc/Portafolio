ae4972ec47a04ec9caa6e15db5616161c16c064c
diegonievsdlc <algorapidonose@gmail.com>
2022-04-18 22:01:47 -0500

    nashe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Menu 
 const btnMenu = document.getElementById('btn-menu')
 const containerLinks = document.querySelector('.nav-links')
+let tamañoPantalla = window.screen.width;
 let contadores = {
     cntNav: false,
     cntCarrusel: true
@@ -17,26 +18,37 @@ btnMenu.addEventListener('click', () =>{
     }
 })
 
+//menu links
+const btnNavLink = document.getElementsByClassName('btn-nav')
+
+for(let i = 0; i < btnNavLink.length; i++){
+  btnNavLink[i].addEventListener('click', function(){
+    var seleccionado = document.getElementsByClassName('active')
+    seleccionado[0].className = seleccionado[0].className.replace(" active")
+    this.className += " active"
+  })
+}
+
 //carrusel mis servicios
-let tamañoPantalla = window.screen.width;
 const carrusel = document.getElementById("mov");
 let cards = document.getElementsByClassName("uwu");
 
 function derecha() {
   if (tamañoPantalla < 768) {
     let cardFirst = document.getElementsByClassName("uwu")[0];
-    carrusel.style.marginLeft = "-10%";
+    carrusel.style.marginLeft = "-73%";
     carrusel.style.transition = "all 0.5s ease-in-out";
     setTimeout(function () {
+      carrusel.style.transition = "none";
       carrusel.insertAdjacentElement("beforeend", cardFirst);
-      carrusel.style.marginLeft = "-1%";
+      carrusel.style.marginLeft = "0";
     }, 500);
   }
 }
 
 setInterval(() => {
   derecha();
-}, 2000);
+}, 3000);
 
 
 //carrusel mis mi portafolio
@@ -46,16 +58,16 @@ let cardsPorta = document.querySelectorAll(".tarao");
 function carruselPorta() {
   if (tamañoPantalla < 768) {
     let cardFirstPorta = document.querySelectorAll(".tarao")[0];
-    cajaPorta.style.marginLeft = "-65%";
+    cajaPorta.style.marginLeft = "-68%";
     cajaPorta.style.transition = "all 0.5s ease-in-out";
     setTimeout(function () {
       cajaPorta.style.transition = "none";
       cajaPorta.insertAdjacentElement("beforeend", cardFirstPorta);
-      cajaPorta.style.marginLeft = "-65%";
+      cajaPorta.style.marginLeft = "0";
     }, 500);
   }
 }
 
 setInterval(function () {
   carruselPorta();
-}, 2000);
+}, 3000);
